de4f47152963f7206ec37cd4f21954950a16b64e
Roberto Rossi <robros@gmail.com>
2024-02-08 16:33:33 +0000

    Created SKPDistributionMILPSolvedInstance

---
+++ src/main/java/skp/milp/instance/SKPDistributionMILPSolvedInstance.java
@@ -0,0 +1,48 @@
+package skp.milp.instance;
+
+import skp.instance.SKPDistribution;
+
+public class SKPDistributionMILPSolvedInstance {
+   public SKPDistribution instance;
+   public int[] optimalKnapsack;
+   public double simulatedSolutionValue;
+   public int simulationRuns;
+   public double milpSolutionValue;
+   public double milpOptimalityGap;
+   public int piecewisePartitions;
+   public int piecewiseSamples;
+   public double milpMaxLinearizationError;
+   public double simulatedLinearizationError;
+   public double cplexSolutionTimeMs;
+   public int simplexIterations;
+   public int exploredNodes;
+   
+   public SKPDistributionMILPSolvedInstance(
+         SKPDistribution instance,
+         int[] optimalKnapsack,
+         double simulatedSolutionValue,
+         int simulationRuns,
+         double milpSolutionValue,
+         double milpOptimalityGap,
+         int piecewisePartitions,
+         int piecewiseSamples,
+         double milpMaxLinearizationError,
+         double simulatedLinearizationError,
+         double cplexSolutionTimeMs,
+         int simplexIterations,
+         int exploredNodes) {
+      this.instance = instance;
+      this.optimalKnapsack = optimalKnapsack;
+      this.simulatedSolutionValue = simulatedSolutionValue;
+      this.simulationRuns = simulationRuns;
+      this.milpSolutionValue = milpSolutionValue;
+      this.milpOptimalityGap = milpOptimalityGap;
+      this.piecewisePartitions = piecewisePartitions;
+      this.piecewiseSamples = piecewiseSamples;
+      this.milpMaxLinearizationError = milpMaxLinearizationError;
+      this.simulatedLinearizationError = simulatedLinearizationError;
+      this.cplexSolutionTimeMs = cplexSolutionTimeMs;
+      this.simplexIterations = simplexIterations;
+      this.exploredNodes = exploredNodes;
+   }
+}
