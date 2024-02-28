38619f3b44c8e5784d1790e65e5282b718486fba
Roberto Rossi <robros@gmail.com>
2024-02-28 13:27:19 +0000

    Created SKPGenericDistributionRecedingSolvedInstance

---
+++ src/main/java/skp/sim/instance/SKPGenericDistributionRecedingSolvedInstance.java
@@ -0,0 +1,39 @@
+package skp.sim.instance;
+
+import skp.instance.SKPGenericDistribution;
+
+public class SKPGenericDistributionRecedingSolvedInstance {
+public SKPGenericDistribution instance;
+   
+   public double simulatedSolutionMean;
+   public double simulatedSolutionStd;
+   public int simulationRuns;
+   
+   public double evp;
+   public double evwpi;
+   public double evwpi_obj_2_n;
+   
+   public int piecewisePartitions;
+   public int piecewiseSamples;
+   
+   public SKPGenericDistributionRecedingSolvedInstance(
+         SKPGenericDistribution instance,
+         double simulatedSolutionMean,
+         double simulatedSolutionStd,
+         int simulationRuns,
+         double evp,
+         double evwpi,
+         double evwpi_obj_2_n,
+         int piecewisePartitions,
+         int piecewiseSamples) {
+      this.instance = instance;
+      this.simulatedSolutionMean = simulatedSolutionMean;
+      this.simulatedSolutionStd = simulatedSolutionStd;
+      this.simulationRuns = simulationRuns;
+      this.evp = evp;
+      this.evwpi = evwpi;
+      this.evwpi_obj_2_n = evwpi_obj_2_n;
+      this.piecewisePartitions = piecewisePartitions;
+      this.piecewiseSamples = piecewiseSamples;
+   }
+}
