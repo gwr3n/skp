9c3d02c1c12916ba9d8382a86af0d41bcdada0c4
Roberto Rossi <robros@gmail.com>
2024-02-29 15:17:39 +0000

    Amended seed

--- src/main/java/skp/batch/SKPBatch.java
+++ src/main/java/skp/batch/SKPBatch.java
@@ -4,6 +4,6 @@ import umontreal.ssj.rng.MRG32k3aL;
 
 public abstract class SKPBatch {
 
-   protected static final long[] seed = {1,2,3,4,5,6};
+   static final long[] seed = {1,2,3,4,5,6};
    protected static final MRG32k3aL randGenerator = new MRG32k3aL();
 }
