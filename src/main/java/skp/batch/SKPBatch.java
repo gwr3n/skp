e88852aec2d5fda9e74c5185d08441df905efe5a
Roberto Rossi <robros@gmail.com>
2024-03-02 10:33:07 +0000

    Updated seed

--- src/main/java/skp/batch/SKPBatch.java
+++ src/main/java/skp/batch/SKPBatch.java
@@ -4,6 +4,6 @@ import umontreal.ssj.rng.MRG32k3aL;
 
 public abstract class SKPBatch {
 
-   static final long[] seed = {123456, 246513, 264531, 425316, 635124, 326451};
+   static final long[] seed = {12345, 24513, 24531, 42531, 35124, 32451};
    protected static final MRG32k3aL randGenerator = new MRG32k3aL();
 }
--- src/main/java/skp/sim/Simulate.java
+++ src/main/java/skp/sim/Simulate.java
@@ -4,7 +4,7 @@ import skp.utilities.probability.SAMPLING;
 import umontreal.ssj.rng.MRG32k3aL;
 
 public abstract class Simulate {
-   static final long[] seed = {123456, 246513, 264531, 425316, 635124, 326451};
+   static final long[] seed = {12345, 24513, 24531, 42531, 35124, 32451};
    MRG32k3aL randGenerator = new MRG32k3aL();
    
    public Simulate() {
