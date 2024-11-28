3146ddc0f3c66c48715bcb8f529aba0b798e5628
Roberto Rossi <robros@gmail.com>
2024-11-28 23:20:16 +0000

    Added cplex.setParam

--- src/main/java/skp/milp/SKPGenericDistributionCuts.java
+++ src/main/java/skp/milp/SKPGenericDistributionCuts.java
@@ -173,6 +173,9 @@ public class SKPGenericDistributionCuts {
       boolean stop = false;
       while(lastKnapsack == null || !stop) {
          IloCplex cplex = new IloCplex();
+         cplex.setParam(IloCplex.Param.TimeLimit, 60);
+         cplex.setParam(IloCplex.Param.Threads, 8);
+         cplex.setParam(IloCplex.Param.MIP.Display, 2);
          cplex.setOut(null);
 
          // Create decision variables
