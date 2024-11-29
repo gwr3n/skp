d4a2ce2d96c43b348c1f926b05572fc9e9ca34a2
Roberto Rossi <robros@gmail.com>
2024-11-29 09:53:49 +0000

    Moved cuts into respective classes

--- src/main/java/skp/milp/SKPGenericDistributionBandB.java
+++ src/main/java/skp/milp/SKPGenericDistributionBandB.java
@@ -268,3 +268,21 @@ class Node {
   }
 }
 
+class Cut {
+   int[] X;
+   double rhs;
+   
+   public Cut(int[] X, double rhs) {
+      this.X = X;
+      this.rhs = rhs;
+   }
+   
+   public int[] getKnapsack() {
+      return X;
+   }
+   
+   public double getRHS() {
+      return rhs;
+   }
+}
+
--- src/main/java/skp/milp/SKPGenericDistributionCuts.java
+++ src/main/java/skp/milp/SKPGenericDistributionCuts.java
@@ -305,3 +305,27 @@ public class SKPGenericDistributionCuts {
       }
    }
 }
+
+class LPNLPCut {
+   double[] X;  // Knapsack
+   double LX;   // Value of loss function at X
+   double[] dd; // Directional derivative
+   
+   public LPNLPCut(double[] X, double LX, double[] dd) {
+      this.X = X;
+      this.LX = LX;
+      this.dd = dd;
+   }
+   
+   public double[] getKnapsack() {
+      return X;
+   }
+   
+   public double getKnapsackLoss() {
+      return this.LX;
+   }
+   
+   public double[] getDirectionalDerivative() {
+      return dd;
+   }
+}
