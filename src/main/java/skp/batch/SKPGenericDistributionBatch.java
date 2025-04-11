219c831697b074e9d2f86a7df6e847583d700acb
Roberto Rossi <robros@gmail.com>
2025-04-11 11:24:19 +0100

    Amended generic experiemntal setup

--- src/main/java/skp/batch/SKPGenericDistributionBatch.java
+++ src/main/java/skp/batch/SKPGenericDistributionBatch.java
@@ -77,7 +77,7 @@ public class SKPGenericDistributionBatch extends SKPBatch {
                   solveMILP(instances, linearizationSamples, maxCuts, simulationRuns, "batch/"+t.toString()+"/"+size+"/"+cv, SolutionMethod.ITERATIVE_CUTS);
                   if(size < instanceSize[3]) // LazyCuts struggles with 500 items in the allotted time limit (no initial solution returned)
                      solveMILP(instances, linearizationSamples, maxCuts, simulationRuns, "batch/"+t.toString()+"/"+size+"/"+cv, SolutionMethod.LAZY_CUTS);
-                  if(size == instanceSize[0])
+                  if(size < instanceSize[2])
                      solveMILP(instances, linearizationSamples, maxCuts, simulationRuns, "batch/"+t.toString()+"/"+size+"/"+cv, SolutionMethod.SAA);
                } catch (IloException e) {
                   e.printStackTrace();
